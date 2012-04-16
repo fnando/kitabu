3e0f0dd344599ea56a422d250b6148a568d36432
Jesse Storimer <jstorimer@gmail.com>
2012-04-15 23:20:20 -0400

    Add prince-friendly footnoting

--- lib/kitabu/parser/pdf.rb
+++ lib/kitabu/parser/pdf.rb
@@ -2,11 +2,32 @@ module Kitabu
   module Parser
     class Pdf < Base
       def parse
-        command = ["prince", html_file.to_s, "-o", pdf_file.to_s]
-        puts command
+        content = File.read(html_file)
+        prince_friendly_footnotes = transform_footnotes(content)
 
+        transformed_html_file = File.new('output/prince_friendly_footnote_file.html', 'w')
+        transformed_html_file.write prince_friendly_footnotes
+
+        command = ["prince", transformed_html_file.path, "-o", pdf_file.to_s]
+        p command
         system(*command)
+
+        transformed_html_file.close
+        File.unlink(transformed_html_file)
         true
+      end
+
+      def transform_footnotes(content)
+        doc = Nokogiri::HTML(content)
+        doc.css('a.footnote').each do |node|
+          prince_friendly_footnote = <<-FN
+            #{node.text}<span class='fn'><a href='#{node['href']}'>#{node['href']}</a></span>
+          FN
+
+          node.replace(prince_friendly_footnote)
+        end
+
+        doc.to_xhtml
       end
 
       def html_file

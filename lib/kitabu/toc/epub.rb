b981eae2425abfee6269edebb4ecbc52f08aaecc
Tiago On SSD Ubuntu <tiago@ti2.net.br>
2016-03-22 15:29:19 -0300

    voltando para 30 caracteres

--- lib/kitabu/toc/epub.rb
+++ lib/kitabu/toc/epub.rb
@@ -27,7 +27,7 @@ module Kitabu
                 <ul>
                   <% navigation.each do |nav| %>
                     <li>
-                      <a href="<%= nav[:content] %>"><%= nav[:label] %></a>
+                      <a href="<%= nav[:content] %>"><%= nav[:label][30] %> </a>
                     </li>
                   <% end %>
                 </ul>

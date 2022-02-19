d249b3ae1985421ec1c07b7ccd9cc2adbc2504d0
Nando Vieira <me@fnando.com>
2022-02-18 19:29:55 -0800

    Bump up version (v3.0.2).

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,6 +1,6 @@
 # Changelog
 
-## Unreleased
+## v3.0.2
 
 - Fix `styles` directory copying.
 
--- lib/kitabu/version.rb
+++ lib/kitabu/version.rb
@@ -4,7 +4,7 @@ module Kitabu
   module Version
     MAJOR = 3
     MINOR = 0
-    PATCH = 1
+    PATCH = 2
     STRING = "#{MAJOR}.#{MINOR}.#{PATCH}"
   end
 end
